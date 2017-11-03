7dec434606488009d0055cabfd9dad4971612342
Mathieu Gauthron <mathieu@matlux.net>
2017-11-03 18:54:33 +0000

    added post examle

--- src/main/scala/net/matlux/HelloWorldService.scala
+++ src/main/scala/net/matlux/HelloWorldService.scala
@@ -3,29 +3,38 @@ package net.matlux
 import akka.actor.ActorSystem
 import akka.event.{Logging, LoggingAdapter}
 import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.{ContentTypes, HttpEntity}
 import akka.stream.ActorMaterializer
-import com.typesafe.config.{ConfigFactory, Config}
+import com.typesafe.config.{Config, ConfigFactory}
 import akka.http.scaladsl.server.Directives._
 
 
 object HelloWorldService {
-  implicit val system = ActorSystem()
+  implicit val system = ActorSystem("my-system")
   implicit val executor = system.dispatcher
   implicit val materializer = ActorMaterializer()
 
   def main(args: Array[String]): Unit = {
     val config = ConfigFactory.load()
     val logger = Logging(system, getClass)
-  
+
     val routes =
+
       path("hello") {
         get {
           complete {
-            "Hello, World!"
+            HttpEntity(ContentTypes.`text/html(UTF-8)`, "<h1>Say hello to akka-http</h1>")
           }
-        }
+        } ~
+          post {
+            complete {
+              HttpEntity(ContentTypes.`application/json`, "{\"say\" :\"hello post to akka-http\"}")
+            }
+          }
+
       }
-  
+
+
     Http().bindAndHandle(routes, config.getString("http.address"), config.getInt("http.port"))
   }
 }
